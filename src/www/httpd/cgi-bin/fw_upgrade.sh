b3b0418ec0d88372b1c20a928cc4da82ada24075
ReOriginAI <jy1486@nyu.edu>
2026-09-15 01:01:17 +0000

    fix updater

--- src/www/httpd/cgi-bin/fw_upgrade.sh
+++ src/www/httpd/cgi-bin/fw_upgrade.sh
@@ -43,20 +43,20 @@ if [ "$VAL" == "info" ] ; then
 
 elif [ "$VAL" == "upgrade" ] ; then
 
+    printf "Content-type: text/plain\r\n\r\n"
+
     FREE_SD=$(df | grep -m1 '/tmp/sd' | grep mmc | awk '{print $4}')
     if [ -z "$FREE_SD" ]; then
-        printf "Content-type: text/html\r\n\r\n"
         printf "No SD detected."
         exit
     fi
 
-    if [ $FREE_SD -lt 100000 ]; then
-        printf "Content-type: text/html\r\n\r\n"
+    if [ "$FREE_SD" -lt 100000 ]; then
         printf "No space left on SD."
         exit
     fi
 
-    # Clean old upgrades
+    # Clean old upgrades.
     rm -rf /tmp/sd/.fw_upgrade
     rm -rf /tmp/sd/.fw_upgrade.conf
     rm -rf /tmp/sd/Factory
@@ -64,42 +64,57 @@ elif [ "$VAL" == "upgrade" ] ; then
 
     mkdir -p /tmp/sd/.fw_upgrade
     mkdir -p /tmp/sd/.fw_upgrade.conf
-    cd /tmp/sd/.fw_upgrade
+    cd /tmp/sd/.fw_upgrade || exit
 
-    MODEL_SUFFIX=`cat $YI_HACK_PREFIX/model_suffix`
-    FW_VERSION=`cat $YI_HACK_PREFIX/version`
-    if [ -f /tmp/sd/${MODEL_SUFFIX}_x.x.x.tgz ]; then
-        mv /tmp/sd/${MODEL_SUFFIX}_x.x.x.tgz /tmp/sd/.fw_upgrade/${MODEL_SUFFIX}_x.x.x.tgz
+    MODEL_SUFFIX=$(cat "$YI_HACK_PREFIX/model_suffix")
+    FW_VERSION=$(cat "$YI_HACK_PREFIX/version")
+    if [ -f "/tmp/sd/${MODEL_SUFFIX}_x.x.x.tgz" ]; then
+        mv "/tmp/sd/${MODEL_SUFFIX}_x.x.x.tgz" "/tmp/sd/.fw_upgrade/${MODEL_SUFFIX}_x.x.x.tgz"
         LATEST_FW="x.x.x"
     else
-        LATEST_FW=`/tmp/sd/yi-hack/usr/bin/wget -O -  https://api.github.com/repos/roleoroleo/yi-hack-Allwinner-v2/releases/latest 2>&1 | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/'`
-        if [ "$FW_VERSION" == "$LATEST_FW" ]; then
-            printf "Content-type: text/html\r\n\r\n"
+        LATEST_FW=$(/tmp/sd/yi-hack/usr/bin/wget -O - https://api.github.com/repos/roleoroleo/yi-hack-Allwinner-v2/releases/latest 2>/dev/null | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+        if [ "$FW_VERSION" = "$LATEST_FW" ]; then
+            rm -rf /tmp/sd/.fw_upgrade /tmp/sd/.fw_upgrade.conf
             printf "No new firmware available."
             exit
         fi
 
-        /tmp/sd/yi-hack/usr/bin/wget https://github.com/roleoroleo/yi-hack-Allwinner-v2/releases/download/$LATEST_FW/${MODEL_SUFFIX}_${LATEST_FW}.tgz
-        if [ ! -f ${MODEL_SUFFIX}_${LATEST_FW}.tgz ]; then
-            printf "Content-type: text/html\r\n\r\n"
+        /tmp/sd/yi-hack/usr/bin/wget -q "https://github.com/roleoroleo/yi-hack-Allwinner-v2/releases/download/$LATEST_FW/${MODEL_SUFFIX}_${LATEST_FW}.tgz" >/dev/null 2>&1
+        if [ ! -f "${MODEL_SUFFIX}_${LATEST_FW}.tgz" ]; then
+            rm -rf /tmp/sd/.fw_upgrade /tmp/sd/.fw_upgrade.conf
             printf "Unable to download firmware file."
             exit
         fi
     fi
 
-    # Backup configuration
-    cp -rf $YI_HACK_PREFIX/etc/* /tmp/sd/.fw_upgrade.conf/
-    rm /tmp/sd/.fw_upgrade.conf/*.tar.gz
+    # Backup configuration.
+    cp -rf "$YI_HACK_PREFIX"/etc/* /tmp/sd/.fw_upgrade.conf/
+    rm -f /tmp/sd/.fw_upgrade.conf/*.tar.gz
 
-    # Prepare new hack
-    tar zxvf ${MODEL_SUFFIX}_${LATEST_FW}.tgz
-    rm ${MODEL_SUFFIX}_${LATEST_FW}.tgz
+    # Prepare the new hack silently. Never let tar write archive member names
+    # to the CGI stream before/among the response body.
+    ARCHIVE="${MODEL_SUFFIX}_${LATEST_FW}.tgz"
+    if ! tar zxf "$ARCHIVE" >/dev/null 2>&1; then
+        rm -rf /tmp/sd/.fw_upgrade /tmp/sd/.fw_upgrade.conf
+        printf "Firmware extraction failed."
+        exit
+    fi
+    rm -f "$ARCHIVE"
+
+    # Never reboot into a partial or wrong-model stage.
+    STAGED_MODEL=$(cat /tmp/sd/.fw_upgrade/yi-hack/model_suffix 2>/dev/null)
+    STAGED_VERSION=$(cat /tmp/sd/.fw_upgrade/yi-hack/version 2>/dev/null)
+    if [ "$STAGED_MODEL" != "$MODEL_SUFFIX" ] || [ -z "$STAGED_VERSION" ] || [ ! -f /tmp/sd/.fw_upgrade/yi-hack/fw_upgrade_in_progress ]; then
+        rm -rf /tmp/sd/.fw_upgrade /tmp/sd/.fw_upgrade.conf
+        printf "Firmware validation failed after extraction."
+        exit
+    fi
+
     mkdir -p /tmp/sd/.fw_upgrade/yi-hack/etc
     cp -rf /tmp/sd/.fw_upgrade.conf/* /tmp/sd/.fw_upgrade/yi-hack/etc/
+    rm -rf /tmp/sd/.fw_upgrade.conf
 
-    # Report the status to the caller
-    printf "Content-type: text/html\r\n\r\n"
-    printf "Download completed, rebooting and upgrading."
+    printf "Firmware staged (%s), rebooting and upgrading." "$STAGED_VERSION"
 
     sync
     sync
